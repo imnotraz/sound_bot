904f54590c44a67674acdf19ced38ceb992bf462
Razvan <razvansalaru@gmail.com>
2020-12-25 18:23:22 +0100

    update zz

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 const discord = require('discord.js')
 const client = new discord.Client()
 const commands = require('./commands.js')
-
+const prefix = require('./config.json')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
-const prefix  = process.env.PREFIX
 const token = process.env.TOKEN
 client.login(token)
 console.log(token)
