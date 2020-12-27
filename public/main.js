3704ad90a146bcc19f0b9f0580d12b43c82f698b
Alberto Botez <botez78@gmail.com>
2020-12-27 20:25:03 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,14 +1,6 @@
 var form = document.getElementById("form");
     var soundInput = document.getElementById("soundInput");
 
-    var soundsTabButton = document.getElementById("soundsTabButton");
-    var youtubeTabButton = document.getElementById("youtubeTabButton");
-
-    var soundsTab = document.getElementById("soundsTab");
-    var youtubeTab = document.getElementById("youtubeTab");
-
-    youtubeTab.classList.add("hidden");
-    soundsTabButton.classList.add("active");
 
     soundInput.addEventListener("keyup", function (e) {
       if (e.keyCode == 13) {
@@ -35,17 +27,3 @@ var form = document.getElementById("form");
         body: data,
       });
     }
-
-    soundsTabButton.addEventListener("click", () => {
-      soundsTab.classList.remove("hidden");
-      youtubeTab.classList.add("hidden");
-      soundsTabButton.classList.add("active");
-      youtubeTabButton.classList.remove("active");
-    });
-
-    youtubeTabButton.addEventListener("click", () => {
-      youtubeTab.classList.remove("hidden");
-      soundsTab.classList.add("hidden");
-      youtubeTabButton.classList.add("active");
-      soundsTabButton.classList.remove("active");
-    });
