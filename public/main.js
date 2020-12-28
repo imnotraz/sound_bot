555d164ed6255b0218d9f1032d837a00d6b810ae
Daniele Lasagno <danielelasagno@gmail.com>
2020-12-28 11:54:54 +0100

    Fixed class names and wrong function calls

--- public/main.js
+++ public/main.js
@@ -1,51 +1,51 @@
-var form = document.getElementById("form");
-    var soundInput = document.getElementById("soundInput");
+const form = document.getElementById("form");
+const soundInput = document.getElementById("sound-input");
 
-    var soundsTabButton = document.getElementById("soundsTabButton");
-    var youtubeTabButton = document.getElementById("youtubeTabButton");
+const soundsTabButton = document.getElementById("sounds-tab-button");
+const youtubeTabButton = document.getElementById("youtube-tab-button");
 
-    var soundsTab = document.getElementById("soundsTab");
-    var youtubeTab = document.getElementById("youtubeTab");
+const soundsTab = document.getElementById("sounds-tab");
+const youtubeTab = document.getElementById("youtube-tab");
 
-    youtubeTab.classList.add("hidden");
-    soundsTabButton.classList.add("active");
+youtubeTab.classList.add("hidden");
+soundsTabButton.classList.add("active");
 
-    soundInput.addEventListener("keyup", function (e) {
-      if (e.keyCode == 13) {
-        sendRequest();
-      }
-    });
+soundInput.addEventListener("keyup", function (e) {
+  if (e.code == "Enter") {
+    sendRequestFromForm();
+  }
+});
 
-    function stop() {
-      fetch("/stop", {
-        method: "post",
-      });
-    }
+function stop() {
+  fetch("/stop", {
+    method: "post",
+  });
+}
 
-    function sendRequestFromFrom() {
-      var val = document.getElementById("soundInput").value;
-      sendRequest(val);
-    }
+function sendRequestFromForm() {
+  const val = document.getElementById("sound-input").value;
+  sendRequest(val);
+}
 
-    function sendRequest(sound) {
-      document.getElementById("sound").value = sound;
-      const data = new URLSearchParams(new FormData(form));
-      fetch("/", {
-        method: "post",
-        body: data,
-      });
-    }
+function sendRequest(sound) {
+  document.getElementById("sound").value = sound;
+  const data = new URLSearchParams(new FormData(form));
+  fetch("/", {
+    method: "post",
+    body: data,
+  });
+}
 
-    soundsTabButton.addEventListener("click", () => {
-      soundsTab.classList.remove("hidden");
-      youtubeTab.classList.add("hidden");
-      soundsTabButton.classList.add("active");
-      youtubeTabButton.classList.remove("active");
-    });
+soundsTabButton.addEventListener("click", () => {
+  soundsTab.classList.remove("hidden");
+  youtubeTab.classList.add("hidden");
+  soundsTabButton.classList.add("active");
+  youtubeTabButton.classList.remove("active");
+});
 
-    youtubeTabButton.addEventListener("click", () => {
-      youtubeTab.classList.remove("hidden");
-      soundsTab.classList.add("hidden");
-      youtubeTabButton.classList.add("active");
-      soundsTabButton.classList.remove("active");
-    });
+youtubeTabButton.addEventListener("click", () => {
+  youtubeTab.classList.remove("hidden");
+  soundsTab.classList.add("hidden");
+  youtubeTabButton.classList.add("active");
+  soundsTabButton.classList.remove("active");
+});
