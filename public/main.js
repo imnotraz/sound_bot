75fbcfccd179355bddcac6899d7e8181152c85e4
Alberto Botez <botez78@gmail.com>
2020-12-31 17:31:50 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,29 +1,29 @@
 var form = document.getElementById("form");
-    var soundInput = document.getElementById("soundInput");
+var soundInput = document.getElementById("soundInput");
 
 
-    soundInput.addEventListener("keyup", function (e) {
-      if (e.keyCode == 13) {
-        sendRequest();
-      }
-    });
+soundInput.addEventListener("keyup", function (e) {
+  if (e.keyCode == 13) {
+    sendRequest();
+  }
+});
 
-    function stop() {
-      fetch("/stop", {
-        method: "post",
-      });
-    }
+function stop() {
+  fetch("/stop", {
+    method: "post",
+  });
+}
 
-    function sendRequestFromFrom() {
-      var val = document.getElementById("soundInput").value;
-      sendRequest(val);
-    }
+function sendRequestFromFrom() {
+  var val = document.getElementById("soundInput").value;
+  sendRequest(val);
+}
 
-    function sendRequest(sound) {
-      document.getElementById("sound").value = sound;
-      const data = new URLSearchParams(new FormData(form));
-      fetch("/", {
-        method: "post",
-        body: data,
-      });
-    }
+function sendRequest(sound) {
+  document.getElementById("sound").value = sound;
+  const data = new URLSearchParams(new FormData(form));
+  fetch("/", {
+    method: "post",
+    body: data,
+  });
+}
